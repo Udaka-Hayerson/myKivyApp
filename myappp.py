43f7b3ccac0775084a01263f1f96ccad838723a9
Udaka-Hayerson <mikehayerson@gmail.com>
2021-10-15 08:47:28 +0300

    Kivy PingPong , больше нечего сказать

--- myappp.py
+++ myappp.py
@@ -1,37 +1,3 @@
-# from kivy.app import App
-# from kivy.uix.widget import Widget
-# from kivy.uix.label import Label
-
-import kivy
-kivy.require('1.0.5')
-
-from kivy.uix.floatlayout import FloatLayout
-from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty
-
-
-class Controller(FloatLayout):
-    '''Create a controller that receives a custom widget from the kv lang file.
-
-    Add an action to be called from the kv lang file.
-    '''
-    label_wid = ObjectProperty()
-    info = StringProperty()
-
-    def do_action(self):
-        self.label_wid.text = 'My label after button press'
-        self.info = 'Vasya was here'
-
-
-class ControllerApp(App):
-
-    def build(self):
-        return Controller(info='who was there?')
-
-
-ControllerApp().run()
-
-
 # *** Незабывайка
 # class _Virtual_Sallary_App(App):
 #     def build(self):
